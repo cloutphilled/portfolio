7f18c0dc1bbb43b264766ef5b3468e4982f6cf53
Phillip Friis Petersen <phillipfriispetersen@gmail.com>
2023-02-10 02:57:18 +0100

    Changed some pictures and added links to bands

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,24 +2,53 @@
   <div class="about">
     <section>
       <img
-      alt="Picture of me"
+      alt="Telos playing at ILTER Festival in 2022"
       class="logo"
-      src="@/assets/Telos_Ilter_RM-08.jpg"
+      src="@/assets/Telos_Ilter_RM-21.jpg"
       height="350"
       loading="eager"
     />
-      <p>Hi! My name is Phillip. I'm a full stack software developer with a big passion for music. I got into coding during my bachelor's degree in "Digital Concept Development".
-        I have a love/hate relationship with coding - meaning that I hate it when the code doesn't work. I'm sure the majority of coders have a similar feeling.
-        When I finally get the code to work, I feel like on top of the world. It's a very euphoric feeling.
+      <p>
+        Hi! My name is Phillip. I'm a full stack software developer with a big
+        passion for music. I got into coding during my bachelor's degree in
+        "Digital Concept Development". I have a love/hate relationship with
+        coding - meaning that I hate it when the code doesn't work. I'm sure the
+        majority of coders have a similar feeling. When I finally get the code
+        to work, I feel like on top of the world. It's a very euphoric feeling.
       </p>
       <br/>
       <p>
-        I get the same feeling whenever I'm playing music. It's quite the addicting feeling. In terms of coding I'm especially passionate about front-end development.
-        I guess it's because it incorporates creativity and logical design. I always feel very proud of myself whenever I'm able to create something that looks cool and feels amazing to use.
+        I get the same feeling whenever I'm playing music. It's quite the
+        addicting feeling. In terms of coding I'm especially passionate about
+        front-end development. I guess it's because it incorporates creativity
+        and logical design. I always feel very proud of myself whenever I'm able
+        to create something that looks cool and feels amazing to use.
       </p>
+      <br/>
+      <p>Check out my bands here:</p>
+      <br/>
+      <div class="row">
+        <div class="column"> 
+          <b>TELOS</b>
+        <br/>
+        <a href="https://telosband.bandcamp.com/" target="_blank">Bandcamp</a>
+        <br/>
+        <a href="https://www.instagram.com/telosband/" target="_blank">Instagram</a>
+        <br/>
+        <a href="https://open.spotify.com/artist/5MNU39WnT8c59UQ3mGmQtp?si=NiGhBoqsQXiA3mcezwxcOg" target="_blank">Spotify</a>
+        <br/>
+      </div>
+      <div class="column">
+      <b>Regarding Ambiguity</b>
+      <br/>
+      <a href="https://regardingambiguity.bandcamp.com/" target="_blank">Bandcamp</a>
+      <br/>
+      <a href="https://www.instagram.com/regardingambiguity/" target="_blank">Instagram</a>
+      <br/>
+      <a href="https://open.spotify.com/artist/3oDCi1wR5W4SnWzCu3qpLc?si=qUTGIyWQT6aNIYDOq2oyig" target="_blank">Spotify</a>
+    </div>
+    </div>
     </section>
-    
-    
   </div>
 </template>
 
@@ -30,5 +59,23 @@
     display: flex;
     align-items: center;
   }
+}
+
+.column {
+  float: left;
+  width: 50%;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.logo {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
