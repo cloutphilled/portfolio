20bdec9aa13f4306518da394597ee7dbde42aab7
Phillip Friis Petersen <phillipfriispetersen@gmail.com>
2024-11-25 18:18:29 +0100

    Fixed the visual issue in 'Toolkit' -> Switched from <ul> to <article> & <p>'s

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -9,66 +9,86 @@ import WelcomeItem from "./WelcomeItem.vue";
       <BIconShareFill class="bi" />
     </template>
     <template #heading>My links</template>
-    <a href="mailto:phillipfriispetersen@gmail.com?subject=Website Inquiry" target="_blank"><BIconEnvelopeFill class="bi"/></a>
+    <a
+      href="mailto:phillipfriispetersen@gmail.com?subject=Website Inquiry"
+      target="_blank"
+      ><BIconEnvelopeFill class="bi"
+    /></a>
     &nbsp
-    <a href="https://github.com/cloutphilled" target="_blank"><BIconGithub class="bi"/></a>
+    <a href="https://github.com/cloutphilled" target="_blank"
+      ><BIconGithub class="bi"
+    /></a>
     &nbsp
-    <a href="https://gitlab.com/cloutphilled" target="_blank"><BIconGit class="bi"/></a>
+    <a href="https://gitlab.com/cloutphilled" target="_blank"
+      ><BIconGit class="bi"
+    /></a>
     &nbsp
-    <a href="https://instagram.com/clout_philled" target="_blank"><BIconInstagram class="bi" /></a>
+    <a href="https://instagram.com/clout_philled" target="_blank"
+      ><BIconInstagram class="bi"
+    /></a>
     &nbsp
-    <a href="https://www.linkedin.com/in/phillip-friis-petersen-2184791b2/" target="_blank"><BIconLinkedin class="bi"/></a>
-
+    <a
+      href="https://www.linkedin.com/in/phillip-friis-petersen-2184791b2/"
+      target="_blank"
+      ><BIconLinkedin class="bi"
+    /></a>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <BIconPersonFill class="bi"/>
+      <BIconPersonFill class="bi" />
     </template>
     <template #heading>Summary</template>
-    <p>Fullstack software developer & Digital Concept Developer who also shreds a mean guitar by making noisy music. Specializing in frontend development, I am passionate about bringing together creative designs with code.</p>
+    <p>
+      Fullstack software developer & Digital Concept Developer who also shreds a
+      mean guitar by making noisy music. Specializing in frontend development, I
+      am passionate about bringing together creative designs with code.
+    </p>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <BIconCodeSquare class="bi"/>
+      <BIconCodeSquare class="bi" />
     </template>
     <template #heading>Toolkit</template>
-      <ul>
-        <li>PowerShell</li>
-        <li>JavaScript</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>TypeScript</li>
-        <li>React</li>
-        <li>Angular</li>
-        <li>Vue</li>
-        <li>Vite</li>
-        <li>VitePress</li>
-        <li>Node.js</li>
-        <li>NEXT.js</li>
-        <li>Front Matter</li>
-        <li>Java</li>
-        <li>SQL/PostgreSQL</li>
-        <li>Bootstrap</li>
-        <li>Tailwind CSS</li>
-        <li>Git</li>
-        <li>Docker</li>
-        <li>Swagger</li>
-        <li>Spring/Springboot</li>
-        <li>Hibernate</li>
-        <li>REST</li>
-        <li>Keycloak</li>
-        <li>Microsoft Azure</li>
-        <li>UX-design</li>
-        <li>Figma</li>
-        <li>AdobeXD</li>
-        <li>AdobePhotoshop</li>
-        <li>AdobePremierePro</li>
-        <li>AdobeInDesign</li>
-        <li>AdobeIllustrator</li>
-        <li>WordPress</li>
-      </ul>  
+    <article>
+      <p>PowerShell</p>
+      <p>Microsoft Azure</p>
+      <p>Microsoft 365</p>
+      <p>Micosoft Intune</p>
+      <p>Sharepoint</p>
+      <p>JavaScript</p>
+      <p>HTML</p>
+      <p>CSS</p>
+      <p>TypeScript</p>
+      <p>Vue</p>
+      <p>Vite</p>
+      <p>VitePress</p>
+      <p>React</p>
+      <p>Angular</p>
+      <p>Node.js</p>
+      <p>NEXT.js</p>
+      <p>Front Matter</p>
+      <p>Java</p>
+      <p>SQL/PostgreSQL</p>
+      <p>Bootstrap</p>
+      <p>Tailwind CSS</p>
+      <p>Git</p>
+      <p>Docker</p>
+      <p>Swagger</p>
+      <p>Spring/Springboot</p>
+      <p>Hibernate</p>
+      <p>REST</p>
+      <p>Keycloak</p>
+      <p>UX-design</p>
+      <p>Figma</p>
+      <p>AdobeXD</p>
+      <p>Adobe Photoshop</p>
+      <p>Adobe PremierePro</p>
+      <p>Adobe InDesign</p>
+      <p>Adobe Illustrator</p>
+      <p>WordPress</p>
+    </article>
   </WelcomeItem>
 
   <WelcomeItem>
@@ -88,16 +108,13 @@ import WelcomeItem from "./WelcomeItem.vue";
   </WelcomeItem> -->
 </template>
 
-
 <style>
-ul{
-    list-style-type: none;
-    -webkit-columns: 3;
-    -moz-columns: 3;
-    columns: 3;
-    margin: 0;
-    justify-content: start;
+article {
+  list-style-type: none;
+  -webkit-columns: 3;
+  -moz-columns: 3;
+  columns: 3;
+  margin: 0;
+  justify-content: start;
 }
-
-
 </style>
